3081afef8b95fc35f80dc02dcc8c281171dbcf0c
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-25 00:03:03 +0530

    GetPairNumberFromColor.cs

--- TelCo.ColorCoder/GetPairNumberFromColor.cs
+++ TelCo.ColorCoder/GetPairNumberFromColor.cs
@@ -21,7 +21,6 @@ namespace TelCo.ColorCoder
                 }
             }
 
-            // Find the minor color in the array and get the index
             int minorIndex = -1;
             for (int i = 0; i < colorMapMinor.Length; i++)
             {
@@ -31,20 +30,15 @@ namespace TelCo.ColorCoder
                     break;
                 }
             }
-            // If colors can not be found throw an exception
+
             if (majorIndex == -1 || minorIndex == -1)
             {
                 throw new ArgumentException(
                     string.Format("Unknown Colors: {0}", pair.ToString()));
             }
 
-            // Compute pair number and Return  
-            // (Note: +1 in compute is because pair number is 1 based, not zero)
             return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
         }
-        /// <summary>
-        /// Test code for the class
-        /// </summary>
-        /// <param name="args"></param>
+
     }
 }
