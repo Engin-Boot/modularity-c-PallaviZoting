5e06d9f8bd5568087fe5ef5fe8090c2edc20d23b
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-25 16:16:15 +0530

    Update MajorMinorColors.cs

--- TelCo.ColorCoder/MajorMinorColors.cs
+++ TelCo.ColorCoder/MajorMinorColors.cs
@@ -9,7 +9,7 @@ namespace TelCo.ColorCoder
         private static Color[] colorMapMajor;
         private static Color[] colorMapMinor;
 
-        static Program()
+        static MajorMinorColors()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
