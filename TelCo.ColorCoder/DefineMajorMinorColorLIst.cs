b8fa29789d695ada6be4b90a2a78e270143cc6c9
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-25 08:57:55 +0530

    Update DefineMajorMinorColor.cs

--- TelCo.ColorCoder/DefineMajorMinorColorLIst.cs
+++ TelCo.ColorCoder/DefineMajorMinorColorLIst.cs
@@ -4,13 +4,7 @@ using System.Drawing;
 
 namespace TelCo.ColorCoder
 {
-    /// <summary>
-    /// The 25-pair color code, originally known as even-count color code, 
-    /// is a color code used to identify individual conductors in twisted-pair 
-    /// wiring for telecommunications.
-    /// This class provides the color coding and 
-    /// mapping of pair number to color and color to pair number.
-    /// </summary>
+
     partial class Program
     {
         /// <summary>
@@ -33,15 +27,12 @@ namespace TelCo.ColorCoder
                 return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
             }
         }
-        /// <summary>
-        /// Static constructor required to initialize static variable
-        /// </summary>
+
         static Program()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
         }
 
-     
     }
 }
