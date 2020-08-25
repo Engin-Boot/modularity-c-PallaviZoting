43881445b723185be2b85d3945ea120ab369b14c
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-25 09:14:31 +0530

    Update test.cs

--- TelCo.ColorCoder/test.cs
+++ TelCo.ColorCoder/test.cs
@@ -4,10 +4,8 @@ using System.Drawing;
 
 namespace TelCo.ColorCoder
 {
-
     class Program
     {
-
         private static void Main(string[] args)
         {
             int pairNumber = 4;
@@ -15,19 +13,6 @@ namespace TelCo.ColorCoder
             Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
             Debug.Assert(testPair1.majorColor == Color.White);
             Debug.Assert(testPair1.minorColor == Color.Brown);
-
-            pairNumber = 5;
-            testPair1 = Program.GetColorFromPairNumber(pairNumber);
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.White);
-            Debug.Assert(testPair1.minorColor == Color.SlateGray);
-
-            pairNumber = 23;
-            testPair1 = Program.GetColorFromPairNumber(pairNumber);
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.Violet);
-            Debug.Assert(testPair1.minorColor == Color.Green);
-
         }
     }
 }
