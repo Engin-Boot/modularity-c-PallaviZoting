5d01423da2d3c83212d24e984be93c491a1a7beb
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-25 17:24:39 +0530

    Update GetColor.cs

--- TelCo.ColorCoder/GetColor.cs
+++ TelCo.ColorCoder/GetColor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
+using static TelCo.ColorCoder;
 using static TelCo.ColorCoder.MajorMinorColors;
 using static TelCo.ColorCoder.ColorPair;
 
