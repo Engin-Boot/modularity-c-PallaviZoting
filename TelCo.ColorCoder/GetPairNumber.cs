89d6a3290640dedc9673ed710f424d404a40bd2a
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-25 16:54:39 +0530

    Update GetPairNumber.cs

--- TelCo.ColorCoder/GetPairNumber.cs
+++ TelCo.ColorCoder/GetPairNumber.cs
@@ -4,7 +4,7 @@ using System.Drawing;
 
 namespace TelCo.ColorCoder
 { 
-    class GetPairNumber:ColorPair
+    class GetPairNumber
     {
         private static int GetPairNumberFromColor(ColorPair pair)
         {
