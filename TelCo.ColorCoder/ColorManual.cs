41b89a8ebce8abdec133e43268db27d58a093518
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-26 20:01:32 +0530

    Create ColorManual.cs

---
+++ TelCo.ColorCoder/ColorManual.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using static TelCo.ColorCoder.MajorMinorColors;
+using static TelCo.ColorCoder.ColorPair;
+
+//print color manual with major minor colors and pair number
+
+namespace TelCo.ColorCoder
+{
+    class ColorManual
+    {
+            public override string ToString() 
+          {
+            string majorminorcolorwithpairnumber = "";
+            int majorcolorlength=colorMapMajor.Length;
+            int minorcolorlength=colorMapMinor.Length;
+            
+            for(int pairNumber = 1; pairNumber <= majorcolorlength*minorcolorlength; pairNumber++)
+            {
+                colorPair majorminorcolorpair = GetColor.GetColorFromPairNumber(pairNumber);
+                Console.WriteLine("Pair Number: , Colors: \n", pairNumber, majorminorcolorpair);
+                majorminorcolorwithpairnumber += "Pair Number : " + pairNumber +  "   Colors : " + majorminorcolorpair + "\n"; 
+            }
+            return majorminorcolorwithpairnumber;
+        
+        }
+    }
+}
