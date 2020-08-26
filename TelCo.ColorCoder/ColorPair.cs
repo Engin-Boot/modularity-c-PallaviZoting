19e3656403cdd14f7270397f69846ab852e4e52f
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-26 19:53:43 +0530

    Update ColorPair.cs

--- TelCo.ColorCoder/ColorPair.cs
+++ TelCo.ColorCoder/ColorPair.cs
@@ -3,33 +3,17 @@ using System.Diagnostics;
 using System.Drawing;
 using static TelCo.ColorCoder.MajorMinorColors;
 
-//gives manual of major minor color list from pair number
-
 namespace TelCo.ColorCoder
 {
     class ColorPair
     {
             internal Color majorColor;
             internal Color minorColor;
-           // public override string ToString()
-            //{
-            //    return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
-            //}
-        
-           public override string ToString() //overrides the ToString() in ColorLists
-          {
-            string majorminorcolorwithpairnumber = "";
-            int majorcolorlength=colorMapMajor.Length;
-            int minorcolorlength=colorMapMinor.Length;
-            
-            for(int pairNumber = 1; pairNumber <= majorcolorlength*minorcolorlength; pairNumber++)
+            public override string ToString()
             {
-                colorPair majorminorcolorpair = GetColor.GetColorFromPairNumber(pairNumber);
-                Console.WriteLine("Pair Number: , Colors: \n", pairNumber, majorminorcolorpai);
-                majorminorcolorwithpairnumber += "Pair Number : " + pairNumber +  "   Colors : " + majorminorcolorpair + "\n"; 
+                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
             }
-            return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
-             return majorminorcolorwithpairnumber;
+        
         }
     }
 }
