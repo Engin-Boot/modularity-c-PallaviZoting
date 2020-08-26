8d8ff88444195855db7fd3d40bbc5031c6fee32f
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-26 20:16:15 +0530

    Create testcolormanual.cs

---
+++ TelCo.ColorCoder/testcolormanual.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using static TelCo.ColorCoder.GetColor;
+using static TelCo.ColorCoder.GetPairNumber;
+using static TelCo.ColorCoder.ColorManual;
+
+namespace TelCo.ColorCoder
+{
+    class testcolormanual
+    {
+        private static void Main(string[] args)
+        {
+            testcolormanual = ColorManual.ToString();
+            Console.WriteLine(testcolormanual);
+        }
+    }
+}
